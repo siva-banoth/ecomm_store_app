b4a385cfa01554f3d2b3f0128df2172b15eefa28
siva-banoth <siva.banoth.95@gmail.com>
2026-07-24 06:38:11 +0530

    added variables

--- src/main/java/com/vector/Application.java
+++ src/main/java/com/vector/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
 
-}
+		int i = 10;
+		int j = 20;
+		
+	}
+	
